81af4b669e9a830e39f37bcdc7aed4119881bf8a
robvagin <robvagin@gmail.com>
2026-05-04 19:37:34 +0300

    Initial Commit

---
+++ GONE/GONEApp.swift
@@ -0,0 +1,17 @@
+//
+//  GONEApp.swift
+//  GONE
+//
+//  Created by Robert Vagin on 04.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct GONEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
